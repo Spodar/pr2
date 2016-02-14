82bd771b40c70908e100fbf5008377bf28ae0b54
Spodar <unknonimus@gmail.com>
2016-02-14 19:13:31 +0200

    example

---
+++ js/engine.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function() {
+	$('.slider').bxSlider({
+		maxSlides: 1,
+		minSlides: 1,
+	    moveSlides: 1,
+	    auto: true,
+	    pause: 3500,
+	    slideMargin: 0,
+	    pager: false,
+	});
+});
